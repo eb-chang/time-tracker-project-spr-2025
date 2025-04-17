f53b4d1b27fece9db0e33cf321e73b7770741edb
Ellie Chang <ebc.chang@wallawalla.edu>
2025-04-17 02:11:59 -0700

    added method prototypes

--- timer_class.hpp
+++ timer_class.hpp
@@ -1,3 +1,6 @@
+#ifndef TIMER_CLASS_H
+#define TIMER_CLASS_H
+
 #include <chrono>
 #include <iostream>
 #include <thread>
@@ -7,7 +10,25 @@ using namespace std;
 
 class Timer {
     private:
-        string timer_type;      // timer, pomodoro, chess, etc
+        int focusTime; // focus time in minutes
     public:
+        //constructors
+        Timer();
+        Timer(int tFocus);
 
+        //setters
+        void setFocusTime(int tFocus);
+
+        //getters
+        int getFocusTime();
+
+        //functions
+        void start();
+        void stop();
+        void cancel();
+
+        //destructor
+        ~Timer();
 };
+
+#endif //TIMER_CLASS_H
