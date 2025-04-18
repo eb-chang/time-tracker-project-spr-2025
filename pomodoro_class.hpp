3dc5841c0ec5d35395c518f2d535b02e7a655f31
elliec <ebc.chang@gmail.com>
2025-04-18 03:38:55 -0700

    added pomodoro class private member variables

--- pomodoro_class.hpp
+++ pomodoro_class.hpp
@@ -1,5 +1,20 @@
 #ifndef POMODORO_CLASS_H
 #define POMODORO_CLASS_H
 
+#include "timer_class.hpp"
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+class Pomodoro : public Timer {
+    private:
+        int breakTime;  // amt of pomodoro rest time
+        int numRep;     // # of timer repetitions (1 rep = 1 complete timer not including break)
+
+    public:
+        
+};
 
 #endif //POMODORO_CLASS_H
