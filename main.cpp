6d43aeec4984fc1d86b748835d165016e1d51c4f
elliec <ebc.chang@gmail.com>
2025-05-07 22:07:30 -0700

    added goodbye message to main

--- main.cpp
+++ main.cpp
@@ -7,11 +7,17 @@
 using namespace std;
 
 int main(){
-    string filename = "test2.bin";
+    string filename = "test4.bin";
     User testUser;
     testUser.startMenu( filename );
+    testUser.adminMenu( filename );
     testUser.timerMenu();
 
     testUser.save( filename );
+
+    cout << "===========================" << endl;
+    cout << "    See you next time!     " << endl;
+    cout << "===========================" << endl;
+    cout << endl;
     return 0;
 }
